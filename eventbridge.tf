7e4beeae0dbcf5c5936e61da5f998442e8e4b70b
Fernando Pedraza <fernando.pedraza@stratusgrid.com>
2023-12-29 17:42:02 -0500

    Update eventbridge.tf
    Fix format

--- eventbridge.tf
+++ eventbridge.tf
@@ -23,7 +23,7 @@ resource "aws_cloudwatch_event_rule" "required_tags" {
       ]
     }
   )
-  name       = "${var.name_prefix}-backup-notifications${local.name_suffix}"
+  name = "${var.name_prefix}-backup-notifications${local.name_suffix}"
 }
 
 resource "aws_cloudwatch_event_target" "aws_backup_to_sns" {
